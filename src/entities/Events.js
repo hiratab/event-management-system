049d2e6c646f60d06324edef38f99e9945605d4c
Bruno Hirata <brn.hirata@gmail.com>
2021-08-28 10:44:02 -0300

    Added Events and Log entities #5

---
+++ src/entities/Events.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class Events {
+    constructor(title, location, maximumCapacity, startDate, endDate, description) {
+        this.title = title
+        this.location = location
+        this.maximumCapacity = maximumCapacity
+        this.startDate = startDate
+        this.endDate = endDate
+        this.description = description
+    }
+}
+
+module.exports = Events
